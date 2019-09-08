6915d6f05f7ca56ad33eedb8535582c0ec5c925b
vigilantserb <eltondzon1001110>
2019-09-08 10:01:34 +0200

    Initial commit

---
+++ app/src/main/java/com/msopensourcedev/timecalculator/main/AddTimeFragment.kt
@@ -0,0 +1,18 @@
+package com.msopensourcedev.timecalculator.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.msopensourcedev.timecalculator.R
+
+class AddTimeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_time, container, false)
+    }
+}
